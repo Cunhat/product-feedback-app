13919b20d6f4ad124452c3ecbeab1b0e3dc3c9d7
Tiago Miguel Cunha <tiago.miguel.cunha@celfocus.com>
2022-08-09 22:10:47 +0100

    Add custom fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'custom-violet': '#AD1FEA',
+      'custom-blue': '#4661E6',
+      'light-blue': '#62BCFA',
+      'dark-blue': '#3A4374',
+      "dark-blue-2": "#373F68",
+      'stone': '#F7F8FD',
+      'light-gray': '#F2F4FF',
+      'light-gray-2': '#CDD2EE',
+      'dark-gray': '#647196',
+      'orange-pastel': '#F49F85'
+    },
+    fontFamily: {
+      regular: 'Jost Regular',
+      bold: 'Jost Bold',
+      semiBold: 'Jost SemiBold',
+    }
+  },
+    
   },
   plugins: [],
 };
