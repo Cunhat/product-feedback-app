5999a22fa57ae27df58721ee5e025b4a52fc4f9c
Tiago Cunha <tiago92cunha@gmail.com>
2022-10-05 16:02:16 +0100

    Fix login page layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -17,7 +17,7 @@ import Thomas from '@/assets/user-images/image-thomas.jpg';
 import Victoria from '@/assets/user-images/image-victoria.jpg';
 import Zena from '@/assets/user-images/image-zena.jpg';
 import Image from 'next/image';
-import { trpc } from '../utils/trpc';
+import { trpc, Users } from '@/utils/trpc';
 
 const images = {
   anne: Anne,
@@ -36,8 +36,31 @@ const images = {
 };
 
 const Login: NextPage = () => {
-  const users = trpc.useQuery(['user.getAllUsers'], { refetchInterval: false, refetchOnReconnect: false, refetchOnWindowFocus: false });
+  const [renderUsers, setRenderUsers] = React.useState<boolean>(false);
+  const router = useRouter();
+  const { status } = useSession();
 
+  if (status === 'authenticated') {
+    router.push('/');
+  }
+
+  return (
+    <div className=' bg-gray-custom py-10 overflow-auto 2xl:h-screen xl:h-screen sm:h-screen flex flex-col px-10 items-center'>
+      {renderUsers ? (
+        <UsersToLogin />
+      ) : (
+        <div className='h-full flex flex-col justify-center items-center gap-20'>
+          <h1 className='font-bold text-2xl text-white'>Welcome to FrontEnd Masters product feedback challenge</h1>
+          <Button text='Login' color='violet' onClick={() => setRenderUsers(!renderUsers)} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
+
+const UsersToLogin: React.FC = () => {
   async function onHandleSubmit(username: String) {
     signIn('credentials', { username: username, callbackUrl: '/' });
   }
@@ -47,26 +70,24 @@ const Login: NextPage = () => {
     return images[index];
   }
 
+  const users = trpc.useQuery(['user.getAllUsers'], { refetchInterval: false, refetchOnReconnect: false, refetchOnWindowFocus: false });
+
   return (
-    <div className=' bg-gray-custom py-10 overflow-auto'>
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className='font-bold text-2xl text-white'>Choose a user to login</h1>
-        <div className='grid md:gap-10 md:grid-cols-3 md:grid-rows-3 xl:gap-20 xl:grid-cols-4 xl:grid-rows-4 md:py-10 xl:py-10 py-10 gap-10'>
-          {users?.data?.map((user) => (
-            <div
-              key={user.id}
-              onClick={() => onHandleSubmit(user.username)}
-              className='flex flex-col gap-3 items-center p-5 rounded-md hover:bg-dark-blue hover:cursor-pointer hover:shadow-xl'
-            >
-              <Image width={100} height={100} className='rounded-full' src={getImageSrc(user.name ?? 'zena')} />
-              <h1 className='font-bold text-[16px] text-white'>{user.name}</h1>
-              <h2 className='font-bold text-[12px] text-custom-violet'>@{user.username}</h2>
-            </div>
-          ))}
-        </div>
+    <>
+      <h1 className='font-bold text-2xl text-white'>Choose a user to login</h1>
+      <div className='grid md:gap-10 md:grid-cols-3 md:grid-rows-3 xl:gap-20 xl:grid-cols-4  md:py-10 xl:py-10 py-10 gap-10 flex-1'>
+        {users?.data?.map((user) => (
+          <div
+            key={user.id}
+            onClick={() => onHandleSubmit(user.username)}
+            className='flex flex-col gap-3 items-center p-5 rounded-md hover:bg-dark-blue hover:cursor-pointer hover:shadow-xl h-fit'
+          >
+            <Image width={100} height={100} className='rounded-full' src={getImageSrc(user.name ?? 'zena')} />
+            <h1 className='font-bold text-[16px] text-white'>{user.name}</h1>
+            <h2 className='font-bold text-[12px] text-pink-500'>@{user.username}</h2>
+          </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 };
-
-export default Login;
